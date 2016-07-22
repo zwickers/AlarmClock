b86f3110bea99ccc79de9df0b169bcaf7e1de82e
justinzwick <zwickyj@gmail.com>
2016-07-22 19:51:58 -0400

    find the view instances from activity_main.xml

--- app/src/main/java/justinzwick/alarmclock/MainActivity.java
+++ app/src/main/java/justinzwick/alarmclock/MainActivity.java
@@ -2,12 +2,32 @@ package justinzwick.alarmclock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gc.materialdesign.views.ButtonRectangle;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ButtonRectangle dateButton;
+    private ButtonRectangle timeButton;
+    private ButtonRectangle alarmButton;
+    private TextView dateTextView;
+    private TextView timeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //finding the view instances from activity_main.xml
+        dateButton = (ButtonRectangle) findViewById(R.id.dateButton);
+        timeButton = (ButtonRectangle) findViewById(R.id.timeButton);
+        alarmButton = (ButtonRectangle) findViewById(R.id.setAlarmButton);
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        timeTextView = (TextView) findViewById(R.id.timeTextView);
+
+
     }
 }
